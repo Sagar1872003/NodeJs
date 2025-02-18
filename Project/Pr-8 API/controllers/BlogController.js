028e2cf79bb32ae1d7da83804c140224f04c014a
prathamm3786 <155967795+prathamm3786@users.noreply.github.com>
2025-02-18 14:22:13 +0530

    api

--- Project/Pr-8 API/controllers/BlogController.js
+++ Project/Pr-8 API/controllers/BlogController.js
@@ -31,11 +31,19 @@ const addBlog = async (req, res) => {
 const adminViewBlog = async (req, res) => {
   try {
     const allBlogs = await blogModel.find({}).populate('authorId');
+    if (!allBlogs.length) {
+      return res.status(200).send({
+        success: true,
+        message: "Blogs Not Available"
+      });
+    }
     return res.status(200).send({
       success: true,
       message: "All Blogs",
-      length: allBlogs.length,
-      allBlogs
+      allBlogs: {
+        length: allBlogs.length,
+        blogs: allBlogs
+      }
     });
   } catch (err) {
     return res.status(500).send({
@@ -47,11 +55,19 @@ const adminViewBlog = async (req, res) => {
 const userViewBlog = async (req, res) => {
   try {
     const blogsOfUser = await blogModel.find({ authorId: req.user?._id });
+    if (!blogsOfUser.length) {
+      return res.status(200).send({
+        success: true,
+        message: "Blogs Not Available"
+      });
+    }
     return res.status(200).send({
       success: true,
       message: "Blogs Of User",
-      length: blogsOfUser.length,
-      blogsOfUser
+      blogsOfUser: {
+        length: blogsOfUser.length,
+        blogs: blogsOfUser
+      }
     });
   } catch (err) {
     return res.status(500).send({
@@ -82,7 +98,13 @@ const adminDeleteBlog = async (req, res) => {
 const userDeleteBlog = async (req, res) => {
   try {
     let id = req.query.id;
-    let single = await blogModel.findById(id);
+    let single = await blogModel.findOne({ _id: id, authorId: req.user?._id });
+    if (!single) {
+      return res.status(400).send({
+        success: false,
+        message: "Blog Not Found"
+      });
+    }
     if (fs.existsSync(single?.image)) {
       fs.unlinkSync(single?.image);
     }
@@ -98,7 +120,75 @@ const userDeleteBlog = async (req, res) => {
     });
   }
 }
+const adminUpdateBlog = async (req, res) => {
+  try {
+    let { id, title, description } = req.body;
+    let single = await blogModel.findById(id);
+    if (req.file) {
+      if (fs.existsSync(single?.image)) {
+        fs.unlinkSync(single?.image);
+      }
+      await blogModel.findByIdAndUpdate(id, {
+        title: title,
+        description: description,
+        image: req.file?.path
+      });
+    } else {
+      await blogModel.findByIdAndUpdate(id, {
+        title: title,
+        description: description,
+        image: single?.image
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      message: "Blog Updated"
+    });
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: err
+    });
+  }
+}
+const userUpdateBlog = async (req, res) => {
+  try {
+    let { id, title, description } = req.body;
+    let single = await blogModel.findOne({ _id: id, authorId: req.user?._id });
+    if (!single) {
+      return res.status(400).send({
+        success: false,
+        message: "Blog Not Found"
+      });
+    }
+    if (req.file) {
+      if (fs.existsSync(single?.image)) {
+        fs.unlinkSync(single?.image);
+      }
+      await blogModel.findByIdAndUpdate(id, {
+        title: title,
+        description: description,
+        image: req.file?.path
+      });
+    } else {
+      await blogModel.findByIdAndUpdate(id, {
+        title: title,
+        description: description,
+        image: single?.image
+      });
+    }
+    return res.status(200).send({
+      success: true,
+      message: "Blog Updated"
+    });
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: err
+    });
+  }
+}
 
 module.exports = {
-  addBlog, adminViewBlog, userViewBlog, adminDeleteBlog,userDeleteBlog
+  addBlog, adminViewBlog, userViewBlog, adminDeleteBlog, userDeleteBlog, adminUpdateBlog, userUpdateBlog
 }
